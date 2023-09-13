3ae2d295de63b0f747a8a41238d48db083a8258f
darko2019 <darko.ilic83@gmail.com>
2023-09-13 23:07:02 +0200

    U JS ubacen reset forme

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 
 });
+// Toast za poslatu poruku u kontakt formi
+// namesteno je na click zbog testa na lokalu u produkciji promeniti na submit
+$(document).ready(function () {
+    $("#form-contact, #form-contact-modal").submit(function () {
+        $(".toast").toast({ delay: 1200 });
+        $(".toast").toast("show");
+        resetForm();
+    });
+});
+// Reset kontakt forme i modal kontakt forme
+function resetForm() {
+    setTimeout(() => {
+        $("#form-contact")[0].reset();
+        $("#form-contact-modal")[0].reset();
+    }, 1000);
+
+    console.log("resetovao sam formu");
+}
